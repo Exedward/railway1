65dbbaad109d231facd47adae22765f0ee11db8c
Eduardo <eduardo.ex7@gmail.com>
2024-08-06 00:34:30 -0300

    Update teste1.py

--- teste1.py
+++ teste1.py
@@ -2,14 +2,9 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/data', methods = ['GET'])
-def insert_data():
-    if request.is_json:
-        data = request.get_json()
-        value = data.get('value')
-        return jsonify({"Mensagem": "Dados inseridos com sucesso!"}), 201
-    else:
-        return jsonify({"Erro": "A requisição deve ser um json"}), 400
+@app.route('/', methods = ['GET'])
+def get_data():
+    return jsonify({"sucesso": "A requisição foi ok!"})
     
 if __name__ == '__name__':
     app.run(debug = True, host = '0.0.0.0', port = 5000)
