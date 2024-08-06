99d8179657c7d4e36e0109854b61b0d930ffb206
Eduardo <eduardo.ex7@gmail.com>
2024-08-06 01:12:20 -0300

    Add files via upload

---
+++ servidor1Railway.py
@@ -0,0 +1,34 @@
+from flask import Flask, request, jsonify
+import psycopg2
+import os
+
+app = Flask(__name__)
+
+#Configuração do banco de dados
+url = os.getenv('DATABASE_URL')
+
+#Conexão com o banco de dados
+def get_db_connection():
+    conn = psycopg2.connect(url, sslmode = 'require')
+    return conn
+
+@app.route('/data', methods = ['POST'])
+def insert_data():
+    if request.is_json:
+        data = request.get_json()
+        value = data.get('value')
+        if not value:
+            return jsonify({"Erro": "Nenhum valor passado."}), 400
+        
+        conn = get_db_connection()
+        cur = conn.cursor()
+        cur.execute('INSERT INTO (Tensão, Corrente)  VALUES (%f)', value)
+        conn.commit()
+        cur.close()
+
+        return jsonify({"Mensagem": "Dados inseridos com sucesso!"}), 201
+    else:
+        return jsonify({"Erro": "A requisição deve ser um json"}), 400
+    
+if __name__ == '__name__':
+    app.run(host = '0.0.0.0', port = 8080)
