bbc5a7a8f7f7c6c9075f6d7bd87163545c4eaa4a
Eduardo <eduardo.ex7@gmail.com>
2024-08-05 23:49:16 -0300

    Add files via upload

---
+++ servidor1RailwayTeste.py
@@ -0,0 +1,15 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/data', methods = ['POST'])
+def insert_data():
+    if request.is_json:
+        data = request.get_json()
+        value = data.get('value')
+        return jsonify({"Mensagem": "Dados inseridos com sucesso!"}), 201
+    else:
+        return jsonify({"Erro": "A requisição deve ser um json"}), 400
+    
+if __name__ == '__name__':
+    app.run(debug = True, host = '0.0.0.0', port = 5000)
