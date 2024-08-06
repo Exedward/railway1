95afa1783fd772c05f2925e31d919ad779069e0f
Eduardo <eduardo.ex7@gmail.com>
2024-08-06 01:06:19 -0300

    Update teste1.py

--- teste1.py
+++ teste1.py
@@ -7,4 +7,4 @@ def get_data():
     return jsonify({"sucesso": "A requisição foi ok!"})
     
 if __name__ == '__name__':
-    app.run(debug = True, host = '0.0.0.0', port = 5000)
+    app.run(host = '0.0.0.0', port = 8080)
